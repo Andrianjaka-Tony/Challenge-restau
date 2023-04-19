f381912b1771ca6e364d97f24c2fb9f282cf0096
Andrianjaka-Tony <andrianjaka.tony@gmail.com>
2023-04-19 12:30:38 +0300

    header animation

---
+++ assets/js/anime-text.js
@@ -0,0 +1,21 @@
+let words = document.querySelectorAll(".animation-text span");
+let title = document.querySelector(".welcome-text");
+
+let property = {
+  transform: "translateY(0)",
+  opacity: 1,
+};
+
+title.animate(property, {
+  duration: 300,
+  fill: "forwards",
+});
+let delay = 500;
+words.forEach((word) => {
+  word.animate(property, {
+    duration: 600,
+    delay: delay,
+    fill: "forwards",
+  });
+  delay += 70;
+});
